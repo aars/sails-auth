276b5851c17913d4901b275ab4155859091041fb
Aaron Heesakkers <aaronheesakkers@gmail.com>
2015-03-27 14:25:21 +0100

    Remove gravatarUrl. Has no place in an auth lib.

--- api/models/User.js
+++ api/models/User.js
@@ -21,16 +21,9 @@ module.exports = {
       via: 'user'
     },
 
-    getGravatarUrl: function () {
-      var md5 = crypto.createHash('md5');
-      md5.update(this.email);
-      return 'https://gravatar.com/avatar/'+ md5.digest('hex');
-    },
-
     toJSON: function () {
       var user = this.toObject();
       delete user.password;
-      user.gravatarUrl = this.getGravatarUrl();
       return user;
     }
   },
